5f9c15caa1716f7ae59a3272becc4d9b446cb050
Kinlly <cristianduarte.ylb@gmail.com>
2021-01-27 15:37:58 +0100

    Header Done

--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,28 @@
 import React from 'react'
-import './Header.css'
+import './Header.css' 
+import Headeroptions from './Headeroptions';
+import SearchIcon from '@material-ui/icons/Search';
+import { BusinessCenter, Chat, Home, Notifications, SupervisorAccount } from '@material-ui/icons';
+
 
 function Header() {
     return (
-        <div className="Header">
-            <h1>This is header test</h1>
-
+        <div className="header">
             <div className="header__left">
-                <img src="" alt=""/>
+                <img src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg" alt=""/>
                 <div className="header__search">
+                    <SearchIcon />
                     <input type="text"/>
-                    {/* icon of search */}
                 </div>
             </div>
             <div className="header__right">
+                <Headeroptions Icon={Home} title="Home"/>
+                <Headeroptions Icon={SupervisorAccount} title="My Network"/>  
+                <Headeroptions Icon={BusinessCenter} title="Jobs"/> 
+                <Headeroptions Icon={Chat} title="Messaging"/> 
+                <Headeroptions Icon={Notifications} title="Notifications"/>
                 
+                <Headeroptions avatar={Notifications} title="me"/>
             </div>            
             
         </div>
