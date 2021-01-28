b479f999920adf5b79324df51cfe3d7847084b86
Kinlly <cristianduarte.ylb@gmail.com>
2021-01-28 10:01:24 +0100

    Sidebar Done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';  
 import './App.css';
-import Header from './Header';
+import Header from './Header'; 
 import Sidebar from './Sidebar';
 
 function App() {
   return (
-    <div className="App"> 
+    <div className="app"> 
       {/* Header */ }
       <Header />
 
       {/* App Body */ }
       <div className="app__body">
-        <Sidebar />  
+        <Sidebar />
       </div>
       {/* Sidebar */ }
       {/* Feeds */ }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React from 'react' 
+import { Avatar } from '@material-ui/core';
+import './Sidebar.css';
 
 function Sidebar() {
+
+    const recentItem = (topic) => (
+        <div className="sidebar__recentItem">
+            <span className="sidebar__hash">#</span>
+            <p>{topic}</p>
+        </div>
+    )
+
+
     return (
         <div className="sidebar">
-            <h1>Im a side bar</h1>
+            <div className="sidebar__top">
+            <img src="https://images.unsplash.com/photo-1496964253150-08379c084a68?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt=""/>
+            <Avatar clssName="sidebar__avatar"/>
+            <h2>Cristian Duarte</h2>
+            <h4>cristianduarte.ylb@gmail.com</h4>    
+            </div>       
+            <div className="sidebar__stats">
+                <div className="sidebar__stat">
+                    <p>Who viewed you</p>
+                    <div className="sidebar__statNumber">32</div>
+                </div>
+                <div className="sidebar__stat">
+                    <p>Contacts</p>
+                    <div className="sidebar__statNumber">1910</div>
+                </div>
+            </div>
+            <div className="sidebar__bottom">
+                <p>Recent</p>
+                {recentItem('reactjs')}
+                {recentItem('programming')}
+                {recentItem('fullstack')}
+                {recentItem('design')}
+                {recentItem('developer')}
+            </div>
         </div>
     )
 }
 
+
 export default Sidebar
+
