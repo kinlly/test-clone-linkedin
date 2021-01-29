01d5ba66e18f6a77e471701c24d04dd747e9f82e
Kinlly <cristianduarte.ylb@gmail.com>
2021-01-29 14:20:17 +0100

    Register Done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import React from 'react';  
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';  
+import { useSelector, useDispatch } from 'react-redux';
 import './App.css';
-import { selectUser } from './features/userSlice';
+import { login, logout, selectUser } from './features/userSlice';
 import Feed from './Feed';
+import { auth } from './firebase';
 import Header from './Header'; 
 import Login from './Login';
 import Sidebar from './Sidebar';
 
 function App() {
   const user = useSelector(selectUser);
-  
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    auth.onAuthStateChanged(userAuth =>{
+      if ( userAuth){
+        //logged in
+        dispatch(login({
+          email: userAuth.email,
+          uid: userAuth.uid,
+          displayName: userAuth.displayName,
+          photoUrl: userAuth.photoUrl,
+        }))
+      }else{
+        //logfed out
+        dispatch(logout());
+      }
+    })
+  },[])
+
   return (
     <div className="app"> 
+    <Header />
       {!user ? (
         <Login />
       ) : (
-       <>
-       <Header />
+       
        <div className="app__body">
          <Sidebar />
          <Feed />
        {/* Widgets */ }
-      </div>
-       </>
+    
+       </div>
+ 
       )
       }
     </div>
--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { login } from './features/userSlice';
+import { auth } from './firebase';
 import './Login.css'
 
 function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
+    const [profilePic, setProfilePic] = useState("");
+    const dispatch = useDispatch();
+
+
+    const loginToApp = (e) => {
+        e.preventDefault();
+    };
+    const register = () => {
+        if (!name){
+            return alert("Please enter a full email");
+        }
+
+        auth.createUserWithEmailAndPassword(email, password).then(
+            (userAuth) => {
+            userAuth.user
+              .updateProfile({
+                displayName: name,
+                photoURL: profilePic,
+              })
+              .then(()=>{
+                dispatch(
+                    login({
+                        email: userAuth.user.email,
+                        uid: userAuth.user.uid,
+                        displayName: name,
+                        photoURL: profilePic,
+                    })
+                  );
+               });
+            }).catch(error => alert(error));
+    };
+
+
     return (
         <div className="login">
-            <h1> You're not logged in </h1>
+            <img src="https://news.hitb.org/sites/default/files/styles/large/public/field/image/500px-LinkedIn_Logo.svg__1.png?itok=q_lR0Vks" alt=""/>
+        
+            <form>
+            <input placeholder="Full name (required if registering)" 
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            type="text"/>
+            <input placeholder="Profile pic URL (optional)" 
+            value={profilePic}
+            onChange = {(e) => setProfilePic(e.target.value)}
+            type="text"/>
+            <input placeholder="Email" 
+            value={email}
+            onChange= { (e) => setEmail(e.target.value)}
+            type="email"/>
+            <input placeholder="Password" 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"/>
+            <button type="submit" onClick={loginToApp}>Sign in</button>
+            </form>
+            <p> Not a member?{" "}
+            <span className="login__register" onClick={register}>Register now!</span>
+            </p>
         </div>
     )
 }
